a276e5241f0e1fc7d4fce04e50186e7e8cb6ac94
Rabbit <515236489@qq.com>
2018-03-07 22:17:00 +0800

    commit Label

---
+++ Label.js
@@ -0,0 +1,33 @@
+/**
+ * Created by Rabbit on 2018/3/7.
+ */
+
+import React, {Component} from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    Image,
+} from 'react-native';
+
+type Props = {};
+export default class Label extends Component<Props> {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+});
